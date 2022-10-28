52355a84eb9c784a1c473fa9dd31e0ff1dc9ed42
Gabriel Brun <lydstyl@gmail.com>
2022-10-28 15:29:31 +0200

    Add react router and protected route.

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,15 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App"
-import "./index.css"
-
+import { BrowserRouter } from "react-router-dom"
 import {
     ApolloClient,
     InMemoryCache,
     ApolloProvider,
     gql,
 } from "@apollo/client"
+import "./index.css"
+
 const client = new ApolloClient({
     uri: "https://flyby-gateway.herokuapp.com/",
     cache: new InMemoryCache(),
@@ -34,7 +35,9 @@ const client = new ApolloClient({
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
         <ApolloProvider client={client}>
-            <App />
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
         </ApolloProvider>
     </React.StrictMode>
 )
