6dd952053c9e7cb22229a4ad7df8704eab1baf99
Gabriel Brun <lydstyl@gmail.com>
2022-10-28 14:43:42 +0200

    Apollo client get started ok.

---
+++ src/main.tsx
@@ -0,0 +1,40 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App"
+import "./index.css"
+
+import {
+    ApolloClient,
+    InMemoryCache,
+    ApolloProvider,
+    gql,
+} from "@apollo/client"
+const client = new ApolloClient({
+    uri: "https://flyby-gateway.herokuapp.com/",
+    cache: new InMemoryCache(),
+})
+
+// // const client = ...
+
+// client
+//     .query({
+//         query: gql`
+//             query GetLocations {
+//                 locations {
+//                     id
+//                     name
+//                     description
+//                     photo
+//                 }
+//             }
+//         `,
+//     })
+//     .then(result => console.log(result))
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+    <React.StrictMode>
+        <ApolloProvider client={client}>
+            <App />
+        </ApolloProvider>
+    </React.StrictMode>
+)
