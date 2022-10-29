812d20bb8a936fb3e479a3eb6242ca3673cd02ec
Gabriel Brun <lydstyl@gmail.com>
2022-10-29 12:20:23 +0200

    Use ref like in apollo doc.

--- src/components/AddTask.tsx
+++ src/components/AddTask.tsx
@@ -4,25 +4,28 @@ import { ADD_TASK } from "../mutations/task"
 import { GET_TASKS } from "../queries/tasks"
 
 function AddTask() {
-    // let input;
-
-    const name = useRef()
+    let input
     const [addTask, { data, loading, error }] = useMutation(ADD_TASK, {
         refetchQueries: [{ query: GET_TASKS }],
     })
     function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
         event.preventDefault()
-        const formData = new FormData(event.currentTarget)
-        const name = formData.get("name") as string
-        addTask({ variables: { name } })
+        addTask({ variables: { name: input.value } })
+        input.value = ""
     }
     if (loading) return "Submitting..."
     if (error) return `Submission error! ${error.message}`
     return (
         <form onSubmit={handleSubmit}>
-            <input type="text" name="name" placeholder="name" />
+            <input
+                ref={node => {
+                    input = node
+                }}
+                type="text"
+                name="name"
+                placeholder="name"
+            />
             <button
-                ref={name}
                 className="mx-4 px-4 border-solid border-2 text-blue-500 border-blue-500 rounded"
                 type="submit"
             >
