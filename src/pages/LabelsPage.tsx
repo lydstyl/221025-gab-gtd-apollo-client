f77eac6de926a0f6c2ec62d405527a3c1ccaacb5
Gabriel Brun <lydstyl@gmail.com>
2022-10-30 23:48:19 +0100

    Sort labels by position in labels page.

--- src/pages/LabelsPage.tsx
+++ src/pages/LabelsPage.tsx
@@ -1,12 +1,25 @@
 import { useQuery } from "@apollo/client"
+import { useState, useEffect } from "react"
 import AddLabel from "../components/AddLabel"
 import H2 from "../components/H2"
 import Label from "../components/Label"
+import { byPosition } from "../domain"
 import { GET_LABELS } from "../queries/label"
 import { Label as LabelType } from "../types/label"
 
 function LabelsPage() {
     const { loading, error, data } = useQuery(GET_LABELS)
+    const [sortedLabels, setSortedLabels] = useState<LabelType[]>([])
+
+    useEffect(() => {
+        if (data?.getLabels) {
+            let labels = [...data.getLabels]
+            labels = labels.sort(byPosition)
+
+            setSortedLabels(labels)
+        }
+    }, [data])
+
     if (loading) return <p>Loading...</p>
     if (error) return <p className="text-red-500">Error: {error.message}</p>
     return (
@@ -15,7 +28,7 @@ function LabelsPage() {
             <ul>
                 <H2>Update labels</H2>
 
-                {data.getLabels.map((label: LabelType) => (
+                {sortedLabels.map((label: LabelType) => (
                     <Label key={label.id} label={label} />
                 ))}
             </ul>
