17c8cd52feb4de7f96b25e242e963e2b512bfdee
Gabriel Brun <lydstyl@gmail.com>
2022-10-30 13:23:47 +0100

    Sort task byTodayOrLessOrNoDateFirst.

--- src/domain/index.ts
+++ src/domain/index.ts
@@ -44,6 +44,32 @@ const byNoDateFirst = (a: Task, b: Task, options?: Options) => {
 
     return 0
 }
+const byTodayOrLessOrNoDateFirst = (a: Task, b: Task, options?: Options) => {
+    if (a.fixedDate) {
+        if (new Date(a.fixedDate) <= new Date()) {
+            return 1
+        }
+    }
+    if (b.fixedDate) {
+        if (new Date(b.fixedDate) <= new Date()) {
+            return 1
+        }
+    }
+    if (!a.fixedDate && b.fixedDate) {
+        if (options?.orderBy === "desc") {
+            return 1
+        }
+        return -1
+    }
+    if (!b.fixedDate && a.fixedDate) {
+        if (options?.orderBy === "desc") {
+            return -1
+        }
+        return 1
+    }
+
+    return 0
+}
 const byLink = (a: Task, b: Task) => {
     if (a.link > b.link) {
         return 1
@@ -53,23 +79,41 @@ const byLink = (a: Task, b: Task) => {
     }
     return 0
 }
-
+const byFirstLabelPosition = (a: Task, b: Task) => {
+    // TODO change this we have to sort label first
+    if (a.labels[0]?.position > b.labels[0]?.position) {
+        return 1
+    }
+    if (a.labels[0]?.position < b.labels[0]?.position) {
+        return -1
+    }
+    return 0
+}
+const byNoLabelFirst = (a: Task, b: Task) => {
+    if (!a.labels.length) {
+        return -1
+    }
+    if (!b.labels.length) {
+        return 1
+    }
+    return 0
+}
 const byCustom = (a: Task, b: Task) => {
-    if (byNoDateFirst(a, b, { orderBy: "asc" })) {
-        return byNoDateFirst(a, b, { orderBy: "asc" })
+    if (byTodayOrLessOrNoDateFirst(a, b, { orderBy: "asc" })) {
+        return byTodayOrLessOrNoDateFirst(a, b, { orderBy: "asc" })
     }
-
-    if (byDate(a, b)) {
-        return byDate(a, b)
+    if (byNoLabelFirst(a, b)) {
+        return byNoLabelFirst(a, b)
     }
-
-    if (byName(a, b, { orderBy: "asc" })) {
-        return byName(a, b, { orderBy: "asc" })
+    if (byFirstLabelPosition(a, b)) {
+        return byFirstLabelPosition(a, b)
     }
-
-    if (byLink(a, b)) {
-        return byLink(a, b)
-    }
+    // if (byName(a, b, { orderBy: "asc" })) {
+    //     return byName(a, b, { orderBy: "asc" })
+    // }
+    // if (byLink(a, b)) {
+    //     return byLink(a, b)
+    // }
 
     return 0
 }
