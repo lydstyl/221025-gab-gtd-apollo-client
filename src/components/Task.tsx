6f69f2ac70c104f015793cd5f27dbe60dbac3a58
Gabriel Brun <lydstyl@gmail.com>
2022-11-10 20:33:38 +0100

    Hilight selected task in task list.

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -8,11 +8,12 @@ function Task({ task }: { task: TaskType }) {
     const [taskDetailId, setTaskDetailId] = useAtom(taskDetailIdAtom)
     const { id, name, link, fixedDate, labels } = task
 
+    const liClasses = `${
+        taskDetailId === id ? "border-red-500" : ""
+    } my-1 border bg-stone-100 sm:ml-4 cursor-pointer`
+
     return (
-        <li
-            className="my-1 border bg-stone-100 sm:ml-4 cursor-pointer"
-            onClick={() => setTaskDetailId(id)}
-        >
+        <li className={liClasses} onClick={() => setTaskDetailId(id)}>
             {fixedDate && (
                 <p className="inline-block bg-stone-300 px-2">
                     {dayjs(new Date(fixedDate)).format("YYYY-MM-DD")}
@@ -30,6 +31,9 @@ function Task({ task }: { task: TaskType }) {
                     </p>
                 ))}
             <p className="inline-block ml-2">{name}</p>
+            <p className="inline-block ml-2">
+                {taskDetailId === id ? "yep" : "nope"}
+            </p>
         </li>
     )
 }
