d3c748cfedf3490c788fad15579b270688835cf3
Gabriel Brun <lydstyl@gmail.com>
2022-11-26 23:32:57 +0100

    Redirect to login when unauthorized.

--- src/hooks/useTasks.tsx
+++ src/hooks/useTasks.tsx
@@ -1,8 +1,17 @@
 import { useQuery } from "@apollo/client"
 import { GET_TASKS } from "../queries/tasks"
+import { useNavigate } from "react-router-dom"
 
 function useTasks() {
-    return useQuery(GET_TASKS)
+    const hook = useQuery(GET_TASKS)
+    let navigate = useNavigate()
+
+    if (hook?.error?.message.toLowerCase().includes("unauthorised")) {
+        localStorage.clear()
+        return navigate("/login")
+    }
+
+    return hook
 }
 
 export default useTasks
