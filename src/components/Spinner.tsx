447827ee6e7ad6e70e1727aa784aad57add95c5f
Gabriel Brun <lydstyl@gmail.com>
2023-01-11 00:02:28 +0100

    Fix loading spinner.

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -4,7 +4,7 @@ import ClipLoader from "react-spinners/ClipLoader"
 const override: CSSProperties = {
     display: "block",
     margin: "0 auto",
-    borderColor: "red",
+    borderColor: "rgb(168,162,158)",
 }
 
 function Spinner() {
@@ -13,13 +13,6 @@ function Spinner() {
 
     return (
         <div className="sweet-loading">
-            <button onClick={() => setLoading(!loading)}>Toggle Loader</button>
-            <input
-                value={color}
-                onChange={input => setColor(input.target.value)}
-                placeholder="Color of the loader"
-            />
-
             <ClipLoader
                 color={color}
                 loading={loading}
