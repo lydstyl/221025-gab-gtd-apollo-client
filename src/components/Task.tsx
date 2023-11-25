6bc3f81987fdfba84450a86ebf838345194022bc
Gabriel Brun <lydstyl@gmail.com>
2023-11-25 02:52:47 +0100

    Display different text color if the day of the week is wednesday or the week-end.

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -3,6 +3,19 @@ import dayjs from "dayjs"
 import { Task as TaskType } from "../types/task"
 import { taskDetailIdAtom } from "../store"
 
+function getClassFromDayOfWeek(dayOfWeek: number) {
+    const weekEnd = [0, 6]
+
+    if (weekEnd.includes(dayOfWeek)) {
+        return "text-stone-600"
+    }
+    if (dayOfWeek === 3) {
+        // wednesday
+        return "text-orange-900"
+    }
+    return ""
+}
+
 function Task({ task }: { task: TaskType }) {
     const [taskDetailId, setTaskDetailId] = useAtom(taskDetailIdAtom)
     const { id, name, link, fixedDate, labels } = task
@@ -11,17 +24,21 @@ function Task({ task }: { task: TaskType }) {
             ? "selected-task border-red-500 border-b-8 border-r-8"
             : ""
     } even:bg-stone-100 odd:bg-stone-300 sm:ml-4 cursor-pointer`
+
+    const date = dayjs(new Date(fixedDate)).format("YYYY-MM-DD")
+    const dayOfWeek = dayjs(new Date(fixedDate)).day()
+
+    const dateClass = `w-28 text-center fixed-date inline-block ${getClassFromDayOfWeek(
+        dayOfWeek
+    )} bg-stone-400 px-2`
+
     return (
         <li
             data-task-id={id}
             className={liClasses}
             onClick={() => setTaskDetailId(id)}
         >
-            {fixedDate && (
-                <p className="w-28 text-center fixed-date inline-block bg-stone-400 px-2">
-                    {dayjs(new Date(fixedDate)).format("YYYY-MM-DD")}
-                </p>
-            )}
+            {fixedDate && <p className={dateClass}>{date}</p>}
 
             {labels &&
                 labels.map(label => (
