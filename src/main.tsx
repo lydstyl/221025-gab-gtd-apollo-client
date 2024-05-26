fd1fe281670f18a15de18383a664f720bdb6e10e
Gabriel Brun <lydstyl@gmail.com>
2024-05-26 12:02:40 +0200

    Change server to render for production.

--- src/main.tsx
+++ src/main.tsx
@@ -1,53 +1,55 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import App from "./App"
-import { BrowserRouter } from "react-router-dom"
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import { BrowserRouter } from 'react-router-dom'
 import {
-    ApolloClient,
-    InMemoryCache,
-    ApolloProvider,
-    createHttpLink,
-} from "@apollo/client"
-import { setContext } from "@apollo/client/link/context"
-import "./index.css"
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink
+} from '@apollo/client'
+import { setContext } from '@apollo/client/link/context'
+import './index.css'
 
 const httpLink = createHttpLink({
-    // uri: "http://localhost:4000/",
-    // uri: process.env.REACT_APP_API_URI,
-    // uri: "https://my-custom-gtd.onrender.com/",
-    uri: "https://lazy-pink-leopard-tux.cyclic.app/",
+  // uri: process.env.REACT_APP_API_URI,
+  // uri: "https://my-custom-gtd.onrender.com/",
+
+  // uri: 'http://localhost:4000/',
+  uri: 'https://two21016-gab-gtd-apollo-server.onrender.com/'
+  // uri: "https://lazy-pink-leopard-tux.cyclic.app/",
 })
 
 const authLink = setContext((_, { headers }) => {
-    const storedLogin = localStorage.getItem("login")
+  const storedLogin = localStorage.getItem('login')
 
-    let token = ""
-    if (storedLogin) {
-        token = JSON.parse(storedLogin).token
-    } else {
-        console.log("Sending request without any token in local storage.")
-    }
+  let token = ''
+  if (storedLogin) {
+    token = JSON.parse(storedLogin).token
+  } else {
+    console.log('Sending request without any token in local storage.')
+  }
 
-    return {
-        headers: {
-            ...headers,
-            authorization: token,
-        },
+  return {
+    headers: {
+      ...headers,
+      authorization: token
     }
+  }
 })
 
 const client = new ApolloClient({
-    link: authLink.concat(httpLink),
+  link: authLink.concat(httpLink),
 
-    cache: new InMemoryCache(),
+  cache: new InMemoryCache()
 })
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-    <React.StrictMode>
-        <ApolloProvider client={client}>
-            <BrowserRouter>
-                <App />
-            </BrowserRouter>
-        </ApolloProvider>
-    </React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ApolloProvider>
+  </React.StrictMode>
 )
