55271d2adda02d853ad78470e8b0aa13d8eb858e
Gabriel Brun <lydstyl@gmail.com>
2024-09-15 21:33:18 +0200

    Fix deprecated warning.

--- src/components/H2.tsx
+++ src/components/H2.tsx
@@ -1,12 +1,15 @@
 interface Props {
-    children: React.ReactNode
-    classes?: string
+  children: React.ReactNode
+  classes?: string
 }
 
-const H2: React.FC<Props> = ({ children, classes }) => {
-    return <h2 className={classes}>{children}</h2>
+const H2: React.FC<Props> = ({
+  children,
+  classes = 'text-xl my-8 text-stone-500'
+}) => {
+  return <h2 className={classes}>{children}</h2>
 }
-H2.defaultProps = {
-    classes: "text-xl my-8 text-stone-500",
-}
+// H2.defaultProps = {
+//     classes: "text-xl my-8 text-stone-500",
+// }
 export default H2
