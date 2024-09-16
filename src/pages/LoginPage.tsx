96590185e45649986687ad0388d370acc5bbc93f
Gabriel Brun <lydstyl@gmail.com>
2024-09-16 08:45:23 +0200

    Try to reset cache after logout.

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,38 +1,47 @@
-import Login from "../components/Login"
-import Button from "../components/Button"
-import CreateAccount from "../components/CreateAccount"
-import DeleteAccount from "../components/DeleteAccount"
-import useHasLoginStored from "../hooks/useHasLoginStored"
-import H2 from "../components/H2"
-import useAuth from "../hooks/useAuth"
-import useLocalAuth from "../hooks/useLocalAuth"
+import Login from '../components/Login'
+import Button from '../components/Button'
+import CreateAccount from '../components/CreateAccount'
+import DeleteAccount from '../components/DeleteAccount'
+import useHasLoginStored from '../hooks/useHasLoginStored'
+import H2 from '../components/H2'
+import useAuth from '../hooks/useAuth'
+import useLocalAuth from '../hooks/useLocalAuth'
+import { useQuery } from '@apollo/client'
+import { GET_TASKS } from '../queries/tasks'
 
 function LoginPage() {
-    const { user, token } = useLocalAuth()
+  const { user, token } = useLocalAuth()
+  const { data, client } = useQuery(GET_TASKS)
 
-    const { signout } = useAuth()
-    const [hasLoginStored, setHasLoginStored] = useHasLoginStored()
+  const { signout } = useAuth()
+  const [hasLoginStored, setHasLoginStored] = useHasLoginStored()
 
-    if (token) {
-        return (
-            <>
-                <H2>Disconnect</H2>
-                <Button options={{ onClick: signout }}>Logout</Button>
+  const logOut = async () => {
+    signout()
+    client.resetStore() // resetting the cache --> @apollo_client.js?v=25652079:311 Uncaught (in promise) ApolloError: Something went wrong. Unauthorised !
+  }
 
-                <div className="border-2 rounded bg-stone-200 -mx-4 p-4 mb-4">
-                    <DeleteAccount />
-                </div>
-            </>
-        )
-    }
-
+  if (token) {
     return (
-        <>
-            <Login />
+      <>
+        <H2>Disconnect</H2>
+        <Button options={{ onClick: signout }}>Logout</Button>
+        {/* <Button options={{ onClick: logOut }}>Logout</Button> */}
 
-            <CreateAccount />
-        </>
+        <div className='border-2 rounded bg-stone-200 -mx-4 p-4 mb-4'>
+          <DeleteAccount />
+        </div>
+      </>
     )
+  }
+
+  return (
+    <>
+      <Login />
+
+      <CreateAccount />
+    </>
+  )
 }
 
 export default LoginPage
